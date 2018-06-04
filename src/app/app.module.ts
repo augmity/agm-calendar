1dd6cefcaaf38acbf211c55d2f88bb46f40ecd5d
TomaszM <tomaszm@enkon.com>
2018-06-04 11:10:56 -0700

    feat: added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import { SharedModule } from './shared';
 
 import { AppComponent } from './app.component';
 
+
 @NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatButtonModule
+  ],
+  declarations: [],
+  exports: [
+    CommonModule,
+    MatButtonModule
+  ],
+  providers: [],
+})
+export class SharedModule {}
