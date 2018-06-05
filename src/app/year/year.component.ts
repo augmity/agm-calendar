ad47e23aabf3058b9f493f9ad65dcc0b6a050134
TomaszM <tomaszm@enkon.com>
2018-06-04 17:33:04 -0700

    feat: year component

--- src/app/year/year.component.ts
+++ src/app/year/year.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
 
 @Component({
-  selector: 'app-year',
+  // tslint:disable-next-line
+  selector: 'year',
   templateUrl: './year.component.html',
   styleUrls: ['./year.component.scss']
 })
 export class YearComponent implements OnInit {
 
+  @Input() year: number;
+  months: number[];
+
   constructor() { }
 
   ngOnInit() {
+    this.months = Array.from({length: 12}, (v, k) => k + 1);
   }
-
 }
